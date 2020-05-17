07a2d68713b0cb43879818bf606f2e3cd5b37ecf
Amirtaraj Ragavan <amragavan@BLR.ALLEGISINDIA.COM>
2020-05-17 23:09:28 +0530

    517finalPush

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,38 +1,18 @@
 import React from 'react';
 import { Jumbotron as Jumbo, Container } from 'react-bootstrap';
-import styled from 'styled-components';
-import boatImage from '../assets/img/header-bg.jpg';
+import lambo from '../assets/img/lambo.png';
 
-const Styles = styled.div`
-  .jumbo {
-    background: url(${boatImage}) no-repeat fixed bottom;
-    background-size: cover;
-    color: #efefef;
-    position: relative;
-    z-index: -2;
-  }
 
-  .overlay {
-    background-color: #000;
-    opacity: 0.5;
-    position: absolute;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    z-index: -1;
-  }
-`;
 
 export const Jumbotron = () => (
-  <Styles>
+ 
     <Jumbo fluid className="jumbo">
       <div className="overlay"></div>
       <Container>
         <br></br>
-        <p>Welcome to my Page</p>
-        <h4>NICE TO SEE YOU AGAIN</h4>
+        <h4>NICE TO SEE YOU AGAIN :)</h4>
+        <img className="img-fluid" src={lambo} alt="header" />
       </Container>
     </Jumbo>
-  </Styles>
+
 )
